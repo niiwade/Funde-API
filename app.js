044d36e4c2fa64024e47052e03b25c8dcf0c8a0d
niiwade <niilantewade77@gmail.com>
2021-04-08 11:46:20 +0000

    express server and routes

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-import express from 'express';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const PORT = 3000;
+
+//init body-parser middleware
+app.use(bodyParser.json());
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+//listen for incoming request
+app.listen(PORT, () => {
+    console.log(`Server is running on  port: http://localhost:${PORT}`);
+});
