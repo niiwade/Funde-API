f5b02d3ff2260cb127478799e70451a1e2b4cd96
niiwade <niilantewade77@gmail.com>
2021-04-09 01:29:44 +0000

    db schema

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        min: 3,
+        max: 20,
+        unique: true
+    },
+
+})
