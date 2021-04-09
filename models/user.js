3a7565518fe84e74162818fff57847fedbe64df3
niiwade <niilantewade77@gmail.com>
2021-04-09 02:21:30 +0000

    user schema

--- models/user.js
+++ models/user.js
@@ -1,12 +1,43 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+    },
+
+    lastname: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true
+
+    },
+
     username: {
         type: String,
-        require: true,
+        required: true,
         min: 3,
         max: 20,
         unique: true
     },
 
-})
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+    },
+
+
+});
