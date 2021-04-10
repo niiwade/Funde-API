5044123878c8691bd4541760f0283dc6e9a57d70
niiwade <niilantewade77@gmail.com>
2021-04-10 01:12:45 +0000

    validate wrong password

--- routes/auth.js
+++ routes/auth.js
@@ -43,6 +43,12 @@ router.post('/login', async(req, res) => {
         //find user
         const user = await User.findOne({ email: req.body.email });
         !user && res.status(404).json("User not found");
+
+        //check for valid password
+        const validatePassword = await bcrypt.compare(req.body.password, user.password);
+        !validatePassword && res.status(400).json("Wrong password");
+
+
     } catch (err) {
         console.log(err);
     }
