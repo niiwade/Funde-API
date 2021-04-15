8727509d17c168df9cae5b585d2186d477cb7ca5
niiwade <niilantewade77@gmail.com>
2021-04-15 02:22:06 +0000

    debug delete  user function

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 
 // routes lead to users     
@@ -10,14 +11,65 @@ router.get('/', (req, res) => {
 
 
 //update user
+router.put("/:id", async(req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Account updated successfully");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can update only your account");
+    }
+});
 
 
 //delete user
+router.delete("/:id", async(req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            await User.findByIdAndDelete({ _id: req.params.id });
+            res.status(200).json("Acoount deleted successfully");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can delete only your account");
+    }
+});
+
 
 //get user
+router.get("/:id", async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 //follow user
+router.put("/:id/follow", async(req, res) => {
+    if (req.body.userId !== req.params.id) {
+
+    } else {
+        res.status(403).json("You cant follow your self|||");
+    }
+});
 
 //unfollow user
 
